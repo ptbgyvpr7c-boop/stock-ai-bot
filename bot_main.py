a0a1203a9cde6c49e528d83c9698a1752f7183d6
ptbgyvpr7c-boop <ptbgyvpr7c@privaterelay.appleid.com>
2026-07-09 13:27:07 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -1,1 +1,10 @@
-bot_main.py
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "🤖 البوت شغال!")
+
+bot.infinity_polling()
